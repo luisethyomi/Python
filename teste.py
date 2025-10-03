92b53b30e06fc1a6f3ebec43b9fe205f0e0e5fe7
luisethyomi <142243350+luisethyomi@users.noreply.github.com>
2025-10-03 12:00:19 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,18 @@
+print("Olá meus amigos")
+
+print(5+3)
+print(5-2)
+print(5*3)
+print(10/2)
+print(3**3)
+
+idade = 17
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+    for i in range(5):
+        print("Número:", i)
+
+        
